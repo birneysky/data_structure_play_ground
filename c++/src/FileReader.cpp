a122c1ce3746e71f930acdb0156dbe408f46fbc9
赵炳东 <zhaobingdong@rongcloud.cn>
2019-01-07 21:00:02 +0800

    m

---
+++ c++/src/FileReader.cpp
@@ -0,0 +1,18 @@
+//
+//  FileReader.cpp
+//  PlayGround
+//
+//  Created by birney on 2019/1/7.
+//  Copyright © 2019年 Grocery. All rights reserved.
+//
+
+#include "FileReader.hpp"
+
+FileReader::FileReader(const std::string& fileName) {
+    this->fileName = fileName;
+}
+
+bool readAllWords(std::vector<std::string>& vec) {
+
+    return true;
+}
---
+++ c++/src/FileReader.hpp
@@ -0,0 +1,23 @@
+//
+//  FileReader.hpp
+//  PlayGround
+//
+//  Created by birney on 2019/1/7.
+//  Copyright © 2019年 Grocery. All rights reserved.
+//
+
+#ifndef FileReader_hpp
+#define FileReader_hpp
+
+#include <iostream>
+#include <string>
+
+class FileReader {
+private:
+    std::string fileName;
+public:
+    FileReader(const std::string& fineName);
+    bool readAllWords(std::vector<std::string>& vec);
+};
+
+#endif /* FileReader_hpp */
