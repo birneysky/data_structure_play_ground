5f9979d83f6027a69e8595f8a86f9cb5b0b31e0c
zhaobingdong🌲🌱🌿💥 <zhaobingdong@rongcloud.cn>
2019-04-29 18:16:55 +0800

    Perfecting MaxHeap add insert and extractMax method for it

--- swift/Playground/Sources/Playground/MaxHeap.swift
+++ swift/Playground/Sources/Playground/MaxHeap.swift
@@ -23,6 +23,19 @@ class MaxHeap<T: Comparable>: CustomStringConvertible {
         shiftUp(count)
     }
 
+	func extractMax() -> T? {
+		guard count > 0 else {
+			return nil
+		}
+
+		let ret = data[1]
+		data.swapAt(1,count)
+		data.removeLast()
+		count = count - 1
+		shiftDown(1)
+		return ret
+	}
+
     private func shiftUp(_ k: Int) {
         var kIndex = k
         while kIndex > 1 && data[kIndex/2] < data[kIndex] {
@@ -32,14 +45,32 @@ class MaxHeap<T: Comparable>: CustomStringConvertible {
     }
 
 
+	private func shiftDown(_ k: Int) {
+		var kIndex = k
+		/// 先保证 索引 k 的节点有左子树
+		while  2 * kIndex <= count {
+			var j = 2 * kIndex
+			/// 先使用 j 记录左孩子的索引，那么 j + 1就表示右孩子，如果节点 kIndex 有右孩子并且大于左孩子，让j 记录右孩子的索引
+			if   j + 1 <=  count && data[j] < data[j+1] {
+				j = j + 1
+			}
+			
+			/// 到这里 j 这个索引记录了左右孩子值较大的那个节点的索引
+			if data[kIndex] >=  data[j] {
+				break
+			}
+			data.swapAt(kIndex,j)
+			kIndex = j
+		}
+	}
+
     // MARK: - CustomStringConvertible
     var description: String {
-        if count <= 1 {
+        if count == 0 {
             return "[]"
         } else {
             return data[1...count].description    
         }
     }
     
-
 }
--- swift/Playground/Sources/Playground/main.swift
+++ swift/Playground/Sources/Playground/main.swift
@@ -1,4 +1,4 @@
-
+import Foundation
 
 if CommandLine.arguments.count != 2 {
 	print("Usage: hello NAME")
@@ -12,5 +12,26 @@ let array = [8,9,10,2,4,5,6,3,21,100]
 for item in array {
     heap.insert(element: item)
 }
+print("heap:\(heap),size:\(heap.size())")
 
-print(heap)
+for _ in 0...8 {
+	guard let max = heap.extractMax()  else {
+		print("heap is empty:\(heap.isEmpty())")
+		throw NSError()
+	}
+	print("max:\(max), heap:\(heap),size:\(heap.size())")
+}
+
+let heap2 = MaxHeap<Int>()
+let array2 = [3,4,2]
+for item in array2 {
+	heap2.insert(element: item)
+}
+
+print("heap2:\(heap2),size:\(heap2.size())")
+for _ in 0..<heap2.size() {
+	guard let max = heap2.extractMax() else {
+		throw NSError()
+	}
+	print("max:\(max), heap2:\(heap2)")
+}
