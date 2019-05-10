cce331c82b5339900402bf160e521bd959b46681
zhaobingdong🌲🌱🌿💥 <zhaobingdong@rongcloud.cn>
2019-05-10 17:25:54 +0800

    complete the  maxProfit function in Solution and write unit tests for it

--- swift/Playground/Tests/SolutionTests/SolutionTests.swift
+++ swift/Playground/Tests/SolutionTests/SolutionTests.swift
@@ -17,4 +17,25 @@ final class SolutionTests: XCTestCase {
         print("\t output->nums2:\(nums2),length:\(length2)")
         XCTAssertEqual(length2, 5)
     }
+    
+    func testSolution_maxProfit() {
+        let s = Solution()
+        let prices1 = [7,1,5,3,6,4]
+        print("\t input->prices1:\(prices1)")
+        let maxProfit1 =  s.maxProfit(prices1)
+        print("\t output->maxProfix1: \(maxProfit1)")
+        XCTAssertEqual(maxProfit1, 7)
+        
+        let prices2 = [1,2,3,4,5]
+        print("\t input->prices2:\(prices2)")
+        let maxProfit2 = s.maxProfit(prices2)
+        print("\t output->maxProfit2: \(maxProfit2)")
+        XCTAssertEqual(maxProfit2, 4)
+        
+        let prices3 = [7,6,4,3,1]
+        print("\t input->prices3:\(prices3)")
+        let maxProfit3 = s.maxProfit(prices3)
+        print("\t output->maxProfit3: \(maxProfit3)")
+        XCTAssertEqual(maxProfit3, 0)
+    }
 }
