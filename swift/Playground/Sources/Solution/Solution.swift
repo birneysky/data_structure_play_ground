1353374547ac4ec7469b61e4e172b390e7a57dad
zhaobingdong🌲🌱🌿💥 <zhaobingdong@rongcloud.cn>
2019-05-23 18:33:26 +0800

    fix bug in longestCommonPrefix

--- swift/Playground/Sources/Solution/Solution.swift
+++ swift/Playground/Sources/Solution/Solution.swift
@@ -710,7 +710,7 @@ public class Solution {
                 }
                 isEqual =  item[index] == first[index]
                 if !isEqual {
-                    break   
+                    break
                 }
             }
             
